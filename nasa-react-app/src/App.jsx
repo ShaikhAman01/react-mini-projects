82c3f7d862c7962a9619a0c7a7760d8e35c10c00
Shaikh Aman <110104595+ShaikhAman01@users.noreply.github.com>
2024-08-31 07:06:15 +0530

    initial commit

---
+++ nasa-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Footer from './components/Footer';
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
+
+function App() {
+
+  return (
+    <>
+    <Sidebar />
+     <Main />
+     <Footer/>
+    </>
+  )
+}
+
+export default App
